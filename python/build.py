183bc7b6bb6dec066b6d6fa475c8b131cdef2344
German Espinosa <germanespinosa@gmail.com>
2021-11-30 09:31:11 -0600

    added current states

--- python/build.py
+++ python/build.py
@@ -1,18 +1,35 @@
+import subprocess
 import sys
 from zipfile import ZipFile
 import glob
+
+lib_name = "agent_tracking_py"
 
 version = ""
 if len(sys.argv) > 1:
     version = "_" + sys.argv[1]
 
-with open("./agent_tracking_py/version.py", "w") as v:
+print("creating new version")
+with open("./" + lib_name + "/version.py", "w") as v:
     v.write("def version():\n\treturn '" + sys.argv[1] + "'\n")
+print("done!")
 
-zipObj = ZipFile('build/agent_tracking_py' + version + '.zip', 'w')
-
-g = glob.glob("./agent_tracking_py/*.py")
+print("building library")
+lib_file = 'build/' + lib_name + version + '.zip'
+zipObj = ZipFile(lib_file, 'w')
+g = glob.glob("./" + lib_name + "/*.py")
 for f in g:
     zipObj.write(f)
 
 zipObj.close()
+print("done!")
+
+print("uploading library" )
+p = subprocess.Popen(["bash","-c", "git add " + lib_file + "; git commit " + lib_file + " -m \"build version " + version + "\" ;git push; echo FINISHED"],
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+o, e = p.communicate()
+
+while b"FINISHED" not in o:
+    pass
+print("done!")
