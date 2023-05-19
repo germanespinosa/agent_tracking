983438c6839c383809101157433f9ecbde0de121
German Espinosa <germanespinosa@gmail.com>
2023-05-19 13:43:44 -0500

    isolate robot simulator

--- python/__info__.py
+++ python/__info__.py
@@ -1,5 +1,5 @@
 def __module_version__():
-	return 0, 0, 15 
+	return 0, 0, 18 
 
 
 def __module_name__():
@@ -60,5 +60,9 @@ def __root_folder__():
 
 def __description__():
 	return '' 
+
+
+def __package_data__():
+	return {} 
 
 
